bd071d0f6e894a2435f251e80d69a358d159bc1d
jfpsb <jfpsb_felipe2@hotmail.com>
2019-06-30 01:42:32 -0300

    Importando Modelos Só funciona por enquanto os modelos do tutorial, nao meu navio =(

--- OpenGLCubo/OpenGLCubo/vshader31.glsl
+++ OpenGLCubo/OpenGLCubo/vshader31.glsl
@@ -1,14 +1,16 @@
 #version 330 core
-layout (location = 0) in vec4 vPosition;
-layout (location = 1) in vec4 vColor;
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
-out vec4 color;
+out vec2 TexCoords;
+
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-uniform mat4 oblique;
 
-void main() {
-	gl_Position = projection * view * oblique * model * vPosition;
-	color = vColor;
+void main()
+{
+    TexCoords = aTexCoords;    
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
